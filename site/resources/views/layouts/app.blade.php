a1641138d04d83c5dec8661deb3d9e413973f2df
Matheus Machado <matheusmachado708@gmail.com>
2018-07-12 17:14:28 -0300

    Besteirinhas da front

--- site/resources/views/layouts/app.blade.php
+++ site/resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Left Side Of Navbar -->
           <ul class="navbar-nav mr-auto">
-
+            <a class="nav-link" href="/home">Home</a>
           </ul>
 
           <!-- Right Side Of Navbar -->
