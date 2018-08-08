4b77279c0851e5d1d09333fbc057c7659fa90a7c
Sekva <matheusmachado708@gmail.com>
2018-08-08 17:39:56 -0300

    Em listar Posts, quantidade de comentários e link pra o perfil do autor

--- site/resources/views/posts/listarPosts.blade.php
+++ site/resources/views/posts/listarPosts.blade.php
@@ -29,14 +29,32 @@
                         </a>
                     @endif
                 </div>
-
-
-                <div class="card-body">{{$post->conteudo}}</div>
-                <div>
-                    <span style="float:right">
-                        <a href="/post/verPost/{{$post->id}}">Ver Post!</a>
-                    </span>
+                @php($elo = \site\User::find($post->id_autor))
+                <div class="card-body">
+                   <div>
+                       <span style="float:right">
+                           <a href="/post/verPost/{{$post->id}}">Ver Post!</a>
+                           <br>
+                           {{\site\Post::find($post->id)->getComentarios()->count()}} comentarios
+                       </span>
+                   </div>
+                   <p> {{$post->conteudo}} </p>
                 </div>
+
+                <span style=" font-size: 13px" >Por <a href="/amigos/ver/{{$elo->id}}">{{$elo->name}}</a></span>
+                <span style=" font-size: 13px" >
+
+                   <?php
+                      $data = '';
+                    ?>
+
+                    @php ($data = $post->created_at)
+
+                   <?php
+                      echo date( "H:i:s", strtotime($data)) . ' do dia ' . date( "d/m/Y", strtotime($data));
+                    ?>
+
+                </span>
             </div>
             <hr>
             <hr>
